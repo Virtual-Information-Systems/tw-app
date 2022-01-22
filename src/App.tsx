46c60e1b33a3afc7fd8b4c25c1110e4d2172c51c
Peter John Smith <pjstarifa@gmail.com>
2022-01-22 11:03:44 +0000

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./tailwind.css";
+import "../node_modules/daisyui/dist/full.css";
 
-function App() {
+interface Props {}
+
+export const App = ({}: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-slate-50">
+        <div className="m-4">
+          <h1 className="text-xl">Hello There World </h1>
+          <button className="btn btn-info btn-xs">Button</button>
+        </div>
+
+        <div className="w-1/2 drop-shadow-lg card lg:card-side">
+          <div className="card-body">
+            <h2 className="card-title">No Images</h2>
+            <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Get Started</button>
+              <button className="btn btn-ghost">More info</button>
+            </div>
+            <div className="bg-slate-50">Hello</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+
+    './src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
+  darkMode: 'media', // or 'class'
+  theme: {
+  },
+  variants: {
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
